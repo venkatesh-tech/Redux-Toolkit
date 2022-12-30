266fe726c3b8d92951784da5e476cb26716a7af8
venkatesh-tech <kolapallinagavenkatesh@gmail.com>
2022-12-30 08:53:00 +0530

    Added extraReducers

--- app/store.js
+++ app/store.js
@@ -1,9 +1,9 @@
 const configureStore = require("@reduxjs/toolkit").configureStore;
-const reduxLogger = require("redux-logger");
+// const reduxLogger = require("redux-logger");
 const cakeReducer = require("../features/cake/cakeSlice");
 const icecreamReducer = require("../features/icecream/icecreamSlice");
 
-const logger = reduxLogger.createLogger();
+// const logger = reduxLogger.createLogger();
 
 //store
 const store = configureStore({
@@ -11,7 +11,7 @@ const store = configureStore({
     cake: cakeReducer,
     icecream: icecreamReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 module.exports = store;
--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const icecreamActions =
   require("./features/icecream/icecreamSlice").icecreamActions;
 
 console.log("Initial state ", store.getState());
-const unsubscribe = store.subscribe(() => {});
-//   console.log("updated state", store.getState());
-// });
+const unsubscribe = store.subscribe(() => {
+  console.log("updated state", store.getState());
+});
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
